1b3d80d05ab413150bede37ba1b6679d088b7fff
kmishmael <kmishmael@gmail.com>
2024-01-11 20:07:17 +0300

    fix: import typing

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 """complex types"""
+from typing import List
 
-
-def sum_list(input_list: list[float]) -> float:
+def sum_list(input_list: List[float]) -> float:
     """sum of a list"""
     sum: float = 0
     for i in input_list:
---
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+"""mixed list of complex types"""
+
+
+def sum_mixed_list(mxd_list: list[float | int]):
+    
