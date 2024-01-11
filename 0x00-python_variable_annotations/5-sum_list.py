38e1176b96426c224fb787527c47f8b8fbcfee80
kmishmael <kmishmael@gmail.com>
2024-01-11 20:05:03 +0300

    fix: wrong return type

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -2,7 +2,7 @@
 """complex types"""
 
 
-def sum_list(input_list: list[float]) -> str:
+def sum_list(input_list: list[float]) -> float:
     """sum of a list"""
     sum: float = 0
     for i in input_list:
