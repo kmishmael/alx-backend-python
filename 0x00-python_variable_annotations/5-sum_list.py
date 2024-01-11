7af442620f1b3898f2540c545f6620afcd29d0da
kmishmael <kmishmael@gmail.com>
2024-01-11 20:02:26 +0300

    chore: complex types -list of floats

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""complex types"""
+
+
+def sum_list(input_list: list[float]) -> str:
+    """sum of a list"""
+    sum: float = 0
+    for i in input_list:
+        sum += i
+    return sum
