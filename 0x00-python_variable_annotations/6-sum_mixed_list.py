9eecc390a55fc3b6018c7a0128daf57d09c7eb2f
kmishmael <kmishmael@gmail.com>
2024-01-11 20:09:26 +0300

    chore: mixed lists

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python3
 """mixed list of complex types"""
+from typing import List
 
 
-def sum_mixed_list(mxd_list: list[float | int]):
-    
+def sum_mixed_list(mxd_list: List[float | int]) -> float:
+    """sum of a list"""
+    sum: float = 0
+    for i in mxd_list:
+        sum += i
+    return sum
