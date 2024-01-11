0564039a38c2d16ae864b1459f92d2c77db1949b
kmishmael <kmishmael@gmail.com>
2024-01-11 19:22:21 +0300

    chore: basic annotations

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,12 @@
+"""add function using type annotations"""
+
+
+def add(a: float, b: float) -> float:
+    """
+    Args
+        a - the first number
+        b - the second number
+    Returns
+        the sum of `a` and `b`
+    """
+    return a + b
