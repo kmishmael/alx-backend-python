aabcddb95f8e0c21922cfd4ad3bd3ec338314924
kmishmael <kmishmael@gmail.com>
2024-01-11 20:10:45 +0300

    chore: validate types

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 """mixed list of complex types"""
-from typing import List
+from typing import List, Union
 
 
-def sum_mixed_list(mxd_list: List[float | int]) -> float:
+def sum_mixed_list(mxd_list: List[Union[float, int]]) -> float:
     """sum of a list"""
     sum: float = 0
     for i in mxd_list:
